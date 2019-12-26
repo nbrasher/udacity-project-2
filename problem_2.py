a8d48f5066b48717cbcb95d5f330dc39fd295c4b
Nathan Brasher <nathan.brasher@bain.com>
2019-12-26 18:31:43 -0500

    complete problem 2

--- problem_2.py
+++ problem_2.py
@@ -2,7 +2,7 @@
 Show me the data structures
 Problem 2: File Recursion
 '''
-
+import os
 
 
 def find_files(suffix, path):
@@ -21,4 +21,31 @@ def find_files(suffix, path):
     Returns:
        a list of paths
     """
-    return None
+    file_list = list()
+
+    if os.path.isdir(path):
+        # Loop through all files and directories
+        for f in os.listdir(path):
+
+            # If item is a file, check if ends with suffix
+            if os.path.isfile(os.path.join(path,f)):
+                if f.endswith(suffix):
+                    file_list.append(os.path.join(path,f))
+
+            # If item is a sub-directory, continue search recursively
+            elif os.path.isdir(os.path.join(path,f)):
+                file_list.extend(find_files(suffix, 
+                                            os.path.join(path, f)))
+
+    return file_list
+
+
+if __name__ == '__main__':
+    file_list = find_files('.c', os.path.join(os.path.dirname(__file__), 
+                                              'testdir'))
+    
+    if file_list:
+        for f in file_list:
+            print(f)
+    else:
+        print('No files found')
